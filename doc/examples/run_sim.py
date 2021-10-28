92f8708ccd600c2970c578c95cc5786c12ed6023
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-10-28 22:42:42 +0200

    Again run_sim.py

--- doc/examples/run_sim.py
+++ doc/examples/run_sim.py
@@ -1,8 +1,10 @@
 """
 To define environmental variable (in the terminal or in your .bashrc)
-use something like: export FLUIDSIM_PATH="/.fsnet/project/meige/2020/20CONVECTION/numerical/"
+use something like: export FLUIDSIM_PATH="/fsnet/project/meige/2020/20CONVECTION/numerical/"
 
 """
+
+import numpy as np
 
 from snek5000_cbox.solver import Simul
 # from phill.solver import Simul
@@ -10,8 +12,10 @@ from snek5000_cbox.solver import Simul
 params = Simul.create_default_params()
 
 aspect_ratio = 1.
-Pr_num = 0.71  # Prandtl number
-Ra_num = 1.0e08  # Rayleigh number
+Pr = 0.71  # Prandtl number
+
+# for aspect ratio 1, Ra_c = 1.825E08
+Ra = 1.83e08  # Rayleigh number
 
 params.output.sub_directory = "examples_cbox"
 
@@ -19,27 +23,41 @@ params.oper.nproc_min = 2
 params.oper.dim = 2
 
 # number of elements
-params.oper.nx = 8
-params.oper.ny = 8
-params.oper.nz = 8
+nb_elements = 8
+params.oper.nx = nb_elements
+params.oper.ny = nb_elements
+params.oper.nz = nb_elements
 
 Lx = params.oper.Lx = 1.0
-params.oper.Ly = Lx * aspect_ratio
+Ly = params.oper.Ly = Lx * aspect_ratio
 
 params.oper.elem.order = 9
 
-coords = params.output.history_points.coords = [(0.5, 0.2), (0.5, 0.8)]
+# creation of the coordinates of the points saved by history points
+n1d = 5
+small = Lx / 10
+
+xs = np.linspace(0, Lx, n1d)
+xs[0] = small
+xs[-1] = Lx - small
+
+ys = np.linspace(0, Ly, n1d)
+ys[0] = small
+ys[-1] = Ly - small
+
+coords = [(x, y) for x in xs for y in ys]
+
+params.output.history_points.coords = coords
 params.oper.max.hist = len(coords) + 1
 
-params.nek.general.user_params = {2: Pr_num, 3: Ra_num}
+params.nek.general.user_params = {2: Pr, 3: Ra}
 
-params.nek.general.num_steps = 20000
-params.nek.general.write_interval = 100
-params.nek.general.dt = 0.005
+params.nek.general.num_steps = 100000
+params.nek.general.write_interval = 1000
+params.nek.general.dt = 0.01
 params.nek.general.time_stepper = "BDF3"
 
 sim = Simul(params)
 
 # sim.make.exec(["run_fg"])
-sim.make.exec(resources={"nproc": 2})
-
+sim.make.exec(["run_fg"], resources={"nproc": 2})
