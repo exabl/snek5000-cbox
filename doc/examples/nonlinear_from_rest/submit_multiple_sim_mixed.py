a49c6b8510c018ffef1b91ee55fe0dd55846da94
khoubana <arman.khoubani@univ-grenoble-alpes.fr>
2022-11-04 13:29:19 +0100

    submit_multiple_sim_mixed.py

--- doc/examples/nonlinear_from_rest/submit_multiple_sim_mixed.py
+++ doc/examples/nonlinear_from_rest/submit_multiple_sim_mixed.py
@@ -6,17 +6,24 @@ prandtl = 0.71
 
 dim = 2
 
-aspect_ratio = 1.0
+if dim == 3:
+    aspect_ratio_z = 4.0
+
+aspect_ratio_y = 1.0
+
+Ra_side_numbs = []
+Ra_vert_numbs = []
+
 nx = 12
 order = 10
 stretch_factor = 0.0
 
 end_time = 3000
 dt = 0.05
-nb_procs = 10
 
-Ra_side = 1000
-Ra_vert = 1000
+nb_procs = 10
+nb_nodes = 1
+walltime = "24:00:00"
 
 z_periodicity = False
 
@@ -33,34 +40,48 @@ cluster.commands_setting_env = [
 ]
 
 
-Ra_side_numbs = np.logspace(np.log10(0.99 * Ra_side), np.log10(1.02 * Ra_side), 5)
-Ra_vert_numbs = np.logspace(np.log10(0.99 * Ra_vert), np.log10(1.02 * Ra_vert), 5)
-
-
-ny = int(nx * aspect_ratio)
-if nx * aspect_ratio - ny:
+ny = int(nx * aspect_ratio_y)
+if nx * aspect_ratio_y - ny:
     raise ValueError
+if dim == 3:
+    nz = int(ny / aspect_ratio_z)
+    if ny / aspect_ratio_z - nz:
+        raise ValueError
 
 for Ra_side_num in Ra_side_numbs:
     for Ra_vert_num in Ra_vert_numbs:
 
         command = (
             f"run_simul.py -Pr {prandtl} -nx {nx} --dim {dim} "
-            f"--order {order} --dt-max {dt} --end-time {end_time} -np {nb_procs} "
-            f"-a_y {aspect_ratio} --stretch-factor {stretch_factor} "
+            f"--order {order} --dt-max {dt} --end-time {end_time} -np {nb_nodes*nb_procs} "
+            f"-a_y {aspect_ratio_y} --stretch-factor {stretch_factor} "
             f"--Ra-side {Ra_side_num} --Ra-vert {Ra_vert_num}"
         )
 
+        if dim == 3:
+            command += f" -a_z {aspect_ratio_z}"
         if z_periodicity:
             command += " --z-periodicity"
 
         print(command)
 
-        name_run = f"MC_asp{aspect_ratio:.3f}_Ra{Ra_side_num:.3e}_{Ra_vert_num:.3e}_Pr{prandtl:.2f}_msh{nx*order}x{round(nx*aspect_ratio)*order}"
+        name_run = (
+            f"MC_asp{aspect_ratio_y:.3f}_Ra{Ra_side_num:.3e}_{Ra_vert_num:.3e}"
+            f"_Pr{prandtl:.2f}_msh{nx*order}x{round(nx*aspect_ratio_y)*order}"
+        )
+
+        if dim == 3:
+            name_run = (
+                f"MC_Ay{aspect_ratio_y:.3f}_Az{aspect_ratio_z:.3f}_Ra{Ra_side_num:.3e}"
+                f"_{Ra_vert_num:.3e}_Pr{prandtl:.2f}_msh{nx*order}x"
+                f"{round(nx*aspect_ratio_y)*order}x{round(ny/aspect_ratio_z)*order}"
+            )
 
         cluster.submit_script(
             command,
             name_run=name_run,
+            walltime=walltime,
+            nb_nodes=nb_nodes,
             nb_cores_per_node=nb_procs,
             omp_num_threads=1,
             ask=False,
