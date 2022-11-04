337285b671ee72f09dcedd15ff27d760038cc6dd
khoubana <arman.khoubani@univ-grenoble-alpes.fr>
2022-11-04 13:47:22 +0100

    run_linear_RB_simple.py

--- doc/examples/run_linear_RB_simple.py
+++ doc/examples/run_linear_RB_simple.py
@@ -6,21 +6,22 @@ from snek5000_cbox.solver import Simul
 
 params = Simul.create_default_params()
 
-aspect_ratio = params.oper.aspect_ratio = 1.0/41
+aspect_ratio = params.oper.aspect_ratio = 1.0 / 6.0
 params.prandtl = 1.0
 
 # for an infinite layer of fluid with Pr = 1.0, the onset of convection is at Ra_c = 1708
-params.Ra_vert = 1720
+params.Ra_vert = 1725
 
-params.output.sub_directory = "examples_cbox/simple/LinRB"
+params.output.sub_directory = "examples_cbox/simple/RB"
 
 params.oper.dim = 2
 
 params.oper.x_periodicity = True
 
-nb_elements = ny = 1
+nb_elements = ny = 2
 params.oper.ny = nb_elements
 nx = params.oper.nx = int(nb_elements / aspect_ratio)
+nx = 20
 params.oper.nz = int(nb_elements / aspect_ratio)
 
 Ly = params.oper.Ly
@@ -28,7 +29,7 @@ Lx = params.oper.Lx = Ly / aspect_ratio
 Lz = params.oper.Lz = Ly / aspect_ratio
 
 
-order = params.oper.elem.order = params.oper.elem.order_out = 12
+order = params.oper.elem.order = params.oper.elem.order_out = 8
 
 params.oper.mesh_stretch_factor = 0.0  # zero means regular
 
@@ -56,21 +57,15 @@ params.nek.general.dt = 0.5
 params.nek.general.time_stepper = "BDF3"
 params.nek.general.stop_at = "endTime"
 params.nek.general.write_control = "runTime"
+params.nek.general.extrapolation = "standard"
 params.nek.general.write_interval = params.nek.general.end_time = 3000
 params.output.history_points.write_interval = 10
 params.output.phys_fields.write_interval_pert_field = 70
-
-# params.nek.general.start_from = "base_flow.restart"
 
 params.nek.problemtype.equation = "incompLinNS"
 params.oper.elem.staggered = "auto"
 params.short_name_type_run = f"lin_Ra{params.Ra_vert:.3e}_{nx*order}x{ny*order}"
 
-# restart_file = "./base_flow_RB_simple.restart"
-params.nek.general.extrapolation = "standard"
-
 sim = Simul(params)
-
-# copyfile(restart_file, sim.params.output.path_session / "base_flow.restart")
 
 sim.make.exec("run_fg", resources={"nproc": 4})
