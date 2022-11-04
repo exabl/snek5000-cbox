cc67a3362fdeab1b4c3649a7c153fb4545e45759
khoubana <arman.khoubani@univ-grenoble-alpes.fr>
2022-11-04 13:26:43 +0100

    submit_multiple_check_RB.py

--- doc/examples/nonlinear_from_rest/submit_multiple_check_RB.py
+++ doc/examples/nonlinear_from_rest/submit_multiple_check_RB.py
@@ -1,21 +1,28 @@
 import numpy as np
 
 from fluiddyn.clusters.legi import Calcul2 as Cluster
-from critical_Ra_RB import Ra_c_RB as Ra_c_RB_tests
 
-prandtl = 1.0
 
 dim = 2
 
-dt_max = 0.005
-end_time = 30
-nb_procs = 10
+if dim == 3:
+    aspect_ratio_z = 4.0
 
-nx = 8
+aspect_ratio_y = 1.0
+
+prandtl = 4.38
+Ra_numbs = [1e9]
+
+nx = 32
 order = 10
 stretch_factor = 0.0
 
-Ra_vert = 1750
+end_time = 3000
+dt = 0.05
+
+nb_procs = 10
+nb_nodes = 1
+walltime = "24:00:00"
 
 x_periodicity = False
 z_periodicity = False
@@ -33,36 +40,50 @@ cluster.commands_setting_env = [
 ]
 
 
-for aspect_ratio, Ra_c_test in Ra_c_RB_tests.items():
+ny = int(nx * aspect_ratio_y)
+if nx * aspect_ratio_y - ny:
+    raise ValueError
+if dim == 3:
+    nz = int(ny / aspect_ratio_z)
+    if ny / aspect_ratio_z - nz:
+        raise ValueError
 
-    ny = int(nx * aspect_ratio)
-    if nx * aspect_ratio - ny:
-        continue
+for Ra_vert_num in Ra_numbs:
 
-    Ra_vert_nums = np.logspace(np.log10(Ra_c_test), np.log10(1.04 * Ra_c_test), 4)
+    command = (
+        f"run_simul_check_from_python.py -Pr {prandtl} -nx {nx} --dim {dim} "
+        f"--order {order} --dt-max {dt} --end-time {end_time} -np {nb_nodes*nb_procs} "
+        f"-a_y {aspect_ratio_y} --stretch-factor {stretch_factor} "
+        f"--Ra-vert {Ra_vert_num}"
+    )
 
-    for Ra_vert_num in Ra_vert_nums:
+    if dim == 3:
+        command += f" -a_z {aspect_ratio_z}"
+    if x_periodicity:
+        command += " --x-periodicity"
+    if z_periodicity:
+        command += " --z-periodicity"
 
-        command = (
-            f"run_simul_check_from_python.py -Pr {prandtl} -nx {nx} --dim {dim} "
-            f"--order {order} --dt-max {dt_max} --end-time {end_time} -np {nb_procs} "
-            f"-a_y {aspect_ratio} --stretch-factor {stretch_factor} "
-            f"--Ra-vert {Ra_vert_num}"
+    print(command)
+
+    name_run = (
+        f"RB_asp{aspect_ratio_y:.3f}_Ra{Ra_vert_num:.3e}_Pr{prandtl:.2f}"
+        f"_msh{nx*order}x{round(nx*aspect_ratio_y)*order}"
+    )
+
+    if dim == 3:
+        name_run = (
+            f"RB_Ay{aspect_ratio_y:.3f}_Az{aspect_ratio_z:.3f}_Ra{Ra_vert_num:.3e}"
+            f"_Pr{prandtl:.2f}_msh{nx*order}x{round(nx*aspect_ratio_y)*order}"
+            f"x{round(ny/aspect_ratio_z)*order}"
         )
 
-        if x_periodicity:
-            command += " --x-periodicity"
-        elif z_periodicity:
-            command += " --z-periodicity"
-
-        print(command)
-
-        name_run = f"RB_asp{aspect_ratio:.3f}_Ra{Ra_vert_num:.3e}_Pr{prandtl:.2f}_msh{nx*order}x{round(nx*aspect_ratio)*order}"
-
-        cluster.submit_script(
-            command,
-            name_run=name_run,
-            nb_cores_per_node=nb_procs,
-            omp_num_threads=1,
-            ask=False,
-        )
+    cluster.submit_script(
+        command,
+        name_run=name_run,
+        walltime=walltime,
+        nb_nodes=nb_nodes,
+        nb_cores_per_node=nb_procs,
+        omp_num_threads=1,
+        ask=False,
+    )
